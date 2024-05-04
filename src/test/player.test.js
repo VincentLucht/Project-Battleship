4708dca6a5efc29ed28a42e64fb5fcbe4644362b
Vincent <vincentlucht04@gmail.com>
2024-05-04 15:21:16 +0200

    Begun Player class and added tests
    - changed eslint rule again
    - syncing

---
+++ src/test/player.test.js
@@ -0,0 +1,35 @@
+const Player = require('../js/player');
+const Gameboard = require('../js/gameboard');
+
+test('Creating a both players with type of "player" and "computer", also have to have their own gameboard', () => {
+  const gameboardPlayer = new Gameboard();
+  const gameboardComputer = new Gameboard();
+
+  const player = new Player('player', gameboardPlayer);
+  const computer = new Player('computer', gameboardComputer);
+
+  expect(player.type).toBe('player');
+  expect(computer.type).toBe('computer');
+});
+
+test('Both players must have a gameboard', () => {
+  // Testing when both type and gameboard are provided
+  expect(new Player('human', 'gameboard')).not.toBe(
+    'Please create a gameboard for the user',
+  );
+
+  // Testing when type is missing
+  expect(new Player(undefined, 'gameboard')).toBe(
+    'Please create a gameboard for the user',
+  );
+
+  // Testing when gameboard is missing
+  expect(new Player('human', undefined)).toBe(
+    'Please create a gameboard for the user',
+  );
+
+  // Testing when both type and gameboard are missing
+  expect(new Player(undefined, undefined)).toBe(
+    'Please create a gameboard for the user',
+  );
+});
